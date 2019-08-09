d3d642038e64e080c29170a1ba9fe45942651df8
Michael Kaip <michael.kaip@student.htw-berlin.de>
2019-08-09 16:04:35 +0200

    SQL Database and WPF with MVVM Project Setup
    SQL Database Setup
    - Added a new database project to the solution
    - Created a profile and published the database
    
    WPF with MVVM Project Setup
    - Added WPF Project to the solution
    - Added a refrence to Caliburn Micro MVVM-Framework
    - Created a ShellViewModel clas as wel as a ShellView window
    - Added a class Bootstrapper.cs to the DesktopUI
    - Applied the necessary configurations to make it work

---
+++ RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+
+namespace RMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+        // Constructor
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<EventAggregator, EventAggregator>();
+
+            // Connecting the ViewModel to the Views using reflection
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewmodelType => _container.RegisterPerRequest(
+                    viewmodelType, viewmodelType.ToString(), viewmodelType));
+        }
+
+        // Enables, that on start up ShellViewModel is getting launched as the base view.
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
