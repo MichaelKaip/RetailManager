2d3d15d4f02f99c4c4f1f784039db3e383ad2e18
Michael Kaip <michael.kaip@student.htw-berlin.de>
2019-08-13 13:34:40 +0200

    Documentation updated
    Added Database Table Creation

--- RMDatabase/dbo/Tables/Product.sql
+++ RMDatabase/dbo/Tables/Product.sql
@@ -5,6 +5,6 @@
     [Description] NVARCHAR(MAX) NOT NULL, 
 	[RetailPrice] MONEY NOT NULL,
     [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
-    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()/* Has to be modified manually everytime
-														 the entry gets modified.*/
+	/* Has to be modified manually everytime the entry gets modified.*/
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
 )
