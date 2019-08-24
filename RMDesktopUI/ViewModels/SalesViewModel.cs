4f622b804bec1b6a4426a29a55befedaec219dd3
Michael Kaip <michael.kaip@student.htw-berlin.de>
2019-08-25 00:36:18 +0200

    Sales PAge Creation
    Added a SalesViewModel.cs and SalesView.xaml to RMDesktopUI, designed the sales page. The SalesViewModel just scaffolds out the SVM, but doesn't hold any logic yet.

---
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        /*
+         * Private backing fields
+         */
+        private BindingList<string> _products;
+        private string _itemQuantity;
+        private BindingList<string> _cart;
+
+        /*
+         * Public Properties
+         */
+        public BindingList<string> Products 
+        {
+            get => _products;
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        public string ItemQuantity
+        {
+            get => _itemQuantity;
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                // Todo: Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public string Tax
+        {
+            get
+            {
+                // Todo: Replace with calculation
+                return "$0.00";
+            }
+        }
+
+        public string Total
+        {
+            get
+            {
+                // Todo: Replace with calculation
+                return "$0.00";
+            }
+        }
+
+
+        public BindingList<string> Cart
+        {
+            get => _cart;
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is selected and an item quantity is given.
+
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure something is selected
+
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                // Make sure there is something in the cart
+
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+
+        }
+
+
+    }
+}
